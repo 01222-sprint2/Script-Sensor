453c8761ce65b78e47017330ce9150f05ced9bcf
Lucas Barroso <lucas.jorge@sptech.school>
2022-09-20 20:31:40 -0300

    Alteração do script do sensor para a utilização apenas do DHT11

--- Arduino_DHT11/Arduino_DHT11.ino
+++ Arduino_DHT11/Arduino_DHT11.ino
@@ -1,29 +1,38 @@
 #include "DHT.h"
-#define dht_type DHT11 
+#define dht_type DHT11 //define qual o tipo de sensor DHTxx que se está utilizando
 
-/** Configurações de Portas e Variáveis **/
+/**
+* Configurações iniciais sobre os sensores
+* DHT11
+*/
 int dht_pin = A2;
-DHT dht_1 = DHT(dht_pin, dht_type);
+DHT dht_1 = DHT(dht_pin, dht_type); //pode-se configurar diversos sensores DHTxx
 
-
-void setup(){
-  Serial.begin(9600);
-  dht_1.begin();
-  pinMode(switch_pin, INPUT);
+void setup()
+{
+Serial.begin(9600);
+dht_1.begin();
 }
 
-void loop(){
-  
-  /**DHT11**/
-  float umidade = dht_1.readHumidity();
-  float temperatura = dht_1.readTemperature();
-  if (isnan(temperatura) or isnan(umidade)){
-    Serial.println("Erro ao ler o DHT");
-  }else{
-    Serial.print("Umidade: " + umidade);
-    Serial.print("; ");
-    Serial.print("Temperatura: " + temperatura);
-    Serial.println(";");
-  }
-    delay(5000);
+void loop()
+{
+/**
+* Bloco do DHT11
+*/
+float umidade = dht_1.readHumidity();
+float temperatura = dht_1.readTemperature();
+if (isnan(temperatura) or isnan(umidade))
+{
+Serial.println("Erro ao ler o DHT");
+}
+else
+{
+Serial.print("Umidade: ");
+Serial.print(umidade);
+Serial.print(";");
+Serial.print("Temperatura: ");
+Serial.print(temperatura);
+Serial.println(";");
+}
+delay(5000);
 }
